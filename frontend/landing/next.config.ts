60bb099c0eaeff01753e3ac9589485990fe0361f
0xblaize <alawodechristopherdolapo2020@gmail.com>
2026-08-29 14:46:56 +0100

    verce

--- frontend/landing/next.config.ts
+++ frontend/landing/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  distDir: '../.next',
   outputFileTracingRoot: path.join(__dirname, '../..'),
 };
 
